6ceae88246d85f36aea0de0b31214a31f164983a
Morris <morris9510@gmail.com>
2023-11-17 16:47:01 -0600

    fix errors and code

--- scriptsPS/PSscripts.js
+++ scriptsPS/PSscripts.js
@@ -2,29 +2,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   //makes a local storage to store my pets array
   let pets = JSON.parse(localStorage.getItem("pets")) || [];
-  //function to display number of pets
-  function displayPetCount() {
-    const countPets = document.getElementById("count");
-    countPets.textContent = pets.length;
-  }
-  //function to display names of pets
-  function displayPetNames() {
-    const petListElement = document.getElementById("petList");
-    petListElement.innerHTML = "";
-    // loop through each pet create a list element in my html to display the name
-    for (let i = 0; i < pets.length; i++) {
-      //create a list element in my index page to display the name
-      const li = document.createElement("li");
-      // changes the list text content to the pets name
-      li.textContent = pets[i].name;
-      //adds the pet name to the list of pets
-      petListElement.appendChild(li);
+  // only run the following functions if we are on the index page
+  if (
+    window.location.pathname === "/index.html" ||
+    window.location.pathname === "/"
+  ) {
+    // function to have pet count and names displayed
+    function displayPetCount() {
+      const countPets = document.getElementById("count");
+      if (countPets) {
+        countPets.textContent = pets.length;
+      }
     }
+
+    function displayPetNames() {
+      const petListElement = document.getElementById("petList");
+      petListElement.innerHTML = "";
+      // Loop through each pet to display the name
+      for (let i = 0; i < pets.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = pets[i].name;
+        petListElement.appendChild(li);
+      }
+    }
+    displayPetCount();
+    displayPetNames();
   }
 
-  // display count and pet names
-  displayPetCount();
-  displayPetNames();
   // constructor for my pet object
   function Pet(name, age, gender, service, breed, type) {
     this.name = name;
@@ -34,6 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     this.breed = breed;
     this.type = type;
   }
+
   // register pet function
   function registerPet() {
     // input values from user
@@ -56,15 +61,13 @@ document.addEventListener("DOMContentLoaded", function () {
     pets.push(newPet);
     localStorage.setItem("pets", JSON.stringify(pets)); // Store registered pets in my  localStorage
 
-    // gets the new count and pet names
-    displayPetCount();
-    displayPetNames();
     // log to the console that i registered their pet then display the values of the new pet
     console.log("Pet registered:", newPet);
     // log the new array to the console
     console.log(pets);
   }
-  //acts like an onclick when i click the button it'll do the register pet function
+
+  // acts like an onclick when I click the button it'll do the register pet function
   document.getElementById("buttonPet").addEventListener("click", function () {
     registerPet();
   });
