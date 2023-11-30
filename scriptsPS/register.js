149a07febff13c4af9d5daa379f8df70eb8e317a
Morris <morris9510@gmail.com>
2023-11-30 17:19:59 -0600

    final project

--- scriptsPS/register.js
+++ scriptsPS/register.js
@@ -1,37 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
   let pets = JSON.parse(localStorage.getItem("pets")) || [];
 
-  function Pet(name, age, gender, service, breed) {}
+  function Pet(name, age, gender, service, breed, type) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.service = service;
+    this.breed = breed;
+    this.type = type;
+  }
+
+  function isValid(aPet) {
+    let validation = true;
+    if (aPet.name === "") {
+      validation = false;
+      document.getElementById("txtName").classList.add("error");
+    } else {
+      document.getElementById("txtName").classList.remove("error");
+    }
+    if (aPet.age === "") {
+      validation = false;
+      document.getElementById("txtAge").classList.add("error");
+    } else {
+      document.getElementById("txtAge").classList.remove("error");
+    }
+    return validation;
+  }
+
   function registerPet() {
-    const inputName = document.getElementById("txtName").value;
-    const inputAge = document.getElementById("txtAge").value;
-    const inputGender = document.getElementById("txtGender").value;
-    const inputService = document.getElementById("txtService").value;
-    const inputBreed = document.getElementById("txtBreed").value;
-    const inputType = document.getElementById("selectType").value;
+    let inputName = document.getElementById("txtName").value;
+    let inputAge = document.getElementById("txtAge").value;
+    let inputGender = document.getElementById("txtGender").value;
+    let inputService = document.getElementById("txtService").value;
+    let inputBreed = document.getElementById("txtBreed").value;
+    let inputType = document.getElementById("selectType").value;
 
-    const newPet = {
-      name: inputName,
-      age: inputAge,
-      gender: inputGender,
-      service: inputService,
-      breed: inputBreed,
-      type: inputType,
-    };
+    let newPet = new Pet(
+      inputName,
+      inputAge,
+      inputGender,
+      inputService,
+      inputBreed,
+      inputType
+    );
 
-    pets.push(newPet);
-    localStorage.setItem("pets", JSON.stringify(pets));
-
-    // Call the function to display the updated list of pets in the table
-    displayRegisteredPets();
-
-    document.getElementById("petForm").reset();
+    if (isValid(newPet)) {
+      pets.push(newPet);
+      localStorage.setItem("pets", JSON.stringify(pets));
+      displayRegisteredPets();
+      document.getElementById("petForm").reset();
+    }
   }
 
   document.getElementById("buttonPet").addEventListener("click", function () {
     registerPet();
   });
-
-  // Display the pets when the page loads
-  displayRegisteredPets();
 });
