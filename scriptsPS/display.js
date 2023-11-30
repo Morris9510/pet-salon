44ae42bc46b120754a7bb546e790dbe8455217e7
Morris <morris9510@gmail.com>
2023-11-29 19:21:18 -0600

    homework

---
+++ scriptsPS/display.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let pets = JSON.parse(localStorage.getItem("pets")) || [];
+
+  function displayRegisteredPets() {
+    const petTableBody = document.getElementById("petTableBody");
+    petTableBody.innerHTML = ""; // Clear previous table content
+
+    for (let i = 0; i < pets.length; i++) {
+      const pet = pets[i];
+      const row = document.createElement("tr");
+
+      // Create cells for each attribute
+      const cells = ["name", "age", "gender", "service", "breed", "type"];
+      cells.forEach((attribute) => {
+        const cell = document.createElement("td");
+        cell.textContent = pet[attribute];
+        row.appendChild(cell);
+      });
+
+      // Create a cell for delete button
+      let deleteCell = document.createElement("td");
+      let deleteButton = document.createElement("button");
+      deleteButton.textContent = `Delete ${pet.name}`;
+      deleteButton.onclick = function () {
+        deletePet(i); // Call delete function with index of the pet
+      };
+      deleteCell.appendChild(deleteButton);
+      row.appendChild(deleteCell);
+
+      // Add the row to the table body
+      petTableBody.appendChild(row);
+    }
+  }
+
+  function deletePet(index) {
+    pets.splice(index, 1); // Remove the pet at the specified index
+    localStorage.setItem("pets", JSON.stringify(pets)); // Update local storage
+    displayRegisteredPets(); // Refresh displayed pets
+  }
+
+  // Call the function to display registered pets when the page loads
+  displayRegisteredPets();
+});
+
+/*  function displayTable(){
+  tr id = ${pet.id}
+  let row=""
+  const TABLE = document.getElementById("Pet table")
+  for ( let i=0; i<pets.length; i++ ){
+    let pet = pets[i];
+    row+='
+    <tr>
+    <td>${pet.nameP}</td>
+    <td>${pet.name}</td>
+    <td>${pet.gender}</td>
+    <td> <button> Delete </button> </td>
+    </tr>
+    `;
+  }
+  table.innerHTML=row;
+} */
---
+++ scriptsPS/register.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let pets = JSON.parse(localStorage.getItem("pets")) || [];
+
+  function Pet(name, age, gender, service, breed) {}
+  function registerPet() {
+    const inputName = document.getElementById("txtName").value;
+    const inputAge = document.getElementById("txtAge").value;
+    const inputGender = document.getElementById("txtGender").value;
+    const inputService = document.getElementById("txtService").value;
+    const inputBreed = document.getElementById("txtBreed").value;
+    const inputType = document.getElementById("selectType").value;
+
+    function validateForm() {
+      const name = document.getElementById("txtName").value;
+      const breed = document.getElementById("txtBreed").value;
+      const service = document.getElementById("txtService").value;
+      const type = document.getElementById("selectType").value;
+
+      if (!name || !breed || !service || type === "Choose Animal") {
+        alert("Please fill out all required fields.");
+        return false; // Prevents form submission if validation fails
+      }
+
+      return true; // Allows form submission if validation passes
+    }
+    const newPet = {
+      name: inputName,
+      age: inputAge,
+      gender: inputGender,
+      service: inputService,
+      breed: inputBreed,
+      type: inputType,
+    };
+
+    pets.push(newPet);
+    localStorage.setItem("pets", JSON.stringify(pets));
+
+    // Call the function to display the updated list of pets in the table
+    displayRegisteredPets();
+
+    document.getElementById("petForm").reset();
+  }
+
+  document.getElementById("buttonPet").addEventListener("click", function () {
+    registerPet();
+  });
+
+  // Display the pets when the page loads
+  displayRegisteredPets();
+});
